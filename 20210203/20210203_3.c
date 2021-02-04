5eefbdda0cd967332762f9f50fab0a55e126ec3c
bonislava <33098504+bonislava@users.noreply.github.com>
2021-02-04 15:13:09 +0200

    Update 20210203_3.c

--- 20210203/20210203_3.c
+++ 20210203/20210203_3.c
@@ -4,3 +4,34 @@
 което търсим. Претърсете масива елемент по елемент и ако
 някой елемент съвпада с търсеното число върнете позицията на
 която се намира този елемент. В противен случай върнете – 1.*/
+#include <stdio.h>
+
+int check();
+int linSearch(int a[], int l, int d);
+
+int main(){
+    int a[] = {1,2,5,6,9,12,14,18};
+    int l = sizeof(a)/4;
+    int d = check();
+
+    if(linSearch(a,l,d) != -1){
+        printf("is in arr on position : %d\n",linSearch(a,l,d));
+    }else{
+        printf("is not in the arr");
+    }
+}
+int check(){
+    int x;
+    printf("Enter num to check \n");
+    scanf("%d",&x);
+    return x;
+}
+int linSearch(int a[], int l, int d){
+    int i;
+    for(i=0; i<l; i++){
+        if(a[i] == d){
+            return i;
+        }
+    }
+    return -1;
+}
