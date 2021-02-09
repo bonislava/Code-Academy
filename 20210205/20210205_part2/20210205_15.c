867829ddecd9a232026be5d221ce231b87151b1c
bonislava <33098504+bonislava@users.noreply.github.com>
2021-02-10 00:01:59 +0200

    Update 20210205_15.c

--- 20210205/20210205_part2/20210205_15.c
+++ 20210205/20210205_part2/20210205_15.c
@@ -1,2 +1,19 @@
 /*Задача 15. Сумирайте елементите на двумерен масив и намерете
 средното по редове и колони. (Представете си, че са ученици в класове).*/
+#include<stdio.h>
+
+int main(){
+	int arr[2][3] = {{1, 2, 5},{6, 7, 9}};
+	int sum, i, j, elements;
+	double avg;
+	elements = 2 * 3;
+	avg = 0.0;
+	for (i = 0; i < 2; i++){ 
+		for (j = 0; j < 3; j++){
+			avg += arr[i][j];
+		}
+	}
+	avg = avg / elements;
+	printf("Average is: %2.f\n", avg);
+	return 0;
+}
