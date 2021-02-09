a273cab16c54c9c3334a629484ad78d7f882474a
bonislava <33098504+bonislava@users.noreply.github.com>
2021-02-09 18:54:10 +0200

    Update 20210208_4.c

--- 20210208/20210208_4.c
+++ 20210208/20210208_4.c
@@ -5,15 +5,15 @@
 
 int main(){
     int i,n;
-    float num[100],sum=0.0,avg;
+    float num[100], sum=0.0, avg;
     printf("Enter the numbers of elements:");
     scanf("%d",&n);
-    while(n>100||n<1){
+    while(n>100 || n<1){
         printf("Error! number should be in range of 1 to 100\n");
         printf("Enter the number of elements again:");
         scanf("%d",&n);
     }
-    for(i=0;i<n;++i){
+    for(i=0; i<n; ++i){
         printf("%d. Enter number:" ,i+1);
         scanf("%f",&num[i]);
         sum += num[i];
