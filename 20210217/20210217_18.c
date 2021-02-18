3f66c20f5a4fbf5164cf5284b7342fbcd395d37f
bonislava <33098504+bonislava@users.noreply.github.com>
2021-02-18 18:43:45 +0200

    Update 20210217_18.c

--- 20210217/20210217_18.c
+++ 20210217/20210217_18.c
@@ -16,3 +16,8 @@ Merge Sort        O(nlogn)       O(nlogn)        O(nlogn)        n           YES
 Radix Sort
 
 Insertion Sort     O(n)          O(n^2)        O(n^2)             1           YES       Insertion     O(n + d), in the worst case over sequences that have d inversions.*/
+
+
+
+/*Merge sort is an efficient, general-purpose, comparison-based sorting algorithm. 
+Most implementations produce a stable sort, which means that the order of equal elements is the same in the input and output*/
