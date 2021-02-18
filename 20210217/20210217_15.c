2ed29622f0d55b36509e772d627752a62597b8c3
bonislava <33098504+bonislava@users.noreply.github.com>
2021-02-18 15:43:25 +0200

    Update 20210217_15.c

--- 20210217/20210217_15.c
+++ 20210217/20210217_15.c
@@ -6,3 +6,45 @@
 2. При всяко завъртане програмата изписва 1 произволно число,
 което не е извадено до момента.
 3. Програмата вади числата, нужни за избраната игра (5 или 6).*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define NUM1 5
+#define NUM2 6
+#define FROM1 35
+#define FROM2 42
+#define FROM3 49
+
+int choice();
+void lottery(int num, int from);
+
+int main(){
+    switch (choice()){
+        case 1: 
+            lottery(NUM1, FROM1);
+            break;
+        case 2:
+            lottery(NUM2, FROM2);
+            break;
+        case 3:
+            lottery(NUM2, FROM3);
+            break;
+    }
+}
+int choice(){
+    int x;
+    printf("For 5/35 press 1.\nFor 6/42 press 2.\nFor 6/49 press 3.\n");
+    scanf("%d", &x);
+    return x;
+}
+void lottery(int num, int from){
+    srand(time(NULL));
+    int result[num];
+    for (int i = 0; i < num; i++){   
+        result[i] = rand() % (from + 1);
+    }
+    printf("Winning numbers :\n");
+    for (int i = 0; i < num; i++){
+        printf("%d \t", result[i]);
+    }
+}
