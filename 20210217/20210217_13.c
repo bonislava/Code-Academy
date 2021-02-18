2680025a4b1aaffe4dc78eec52860e4bc09ae4a2
bonislava <33098504+bonislava@users.noreply.github.com>
2021-02-18 15:11:19 +0200

    Update 20210217_13.c

--- 20210217/20210217_13.c
+++ 20210217/20210217_13.c
@@ -1,4 +1,45 @@
 /*Напишете програма, която да прочете низ от потребителя и да го обърне
 в обратен ред.
 Например: 'xyz' да стане 'zyx'*/
+#include <stdio.h>
+#include <string.h>
+#define SIZE 10
 
+char *rev(char *arr);
+int length(char *arr);
+
+int main(void){
+    char str[SIZE];
+    printf("Enter a string:\n");
+    gets(str);
+    printf("Your string is: \n");
+    puts(str);
+    char *p;
+    p = rev(str);
+    printf("reversed: \n");
+    puts(p);
+    return 0;
+}
+char *rev(char *arr){
+    int i;
+    char *x,*y,temp;
+    y = arr;
+    while(*y){
+        y++;
+    }
+    if(arr != y){
+        for(x = arr; x < --y; ++x){
+            temp = *y;
+            *y = *x;
+            *x = temp;
+        }
+    }
+    return arr;
+}
+int length(char *arr){
+    int length;
+    while(*(arr+length) != '\0'){
+        length++;
+    }
+    return length;
+}
