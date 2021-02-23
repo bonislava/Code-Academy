e1d134d6968497b452fa8104e81d471934d25a81
bonislava <33098504+bonislava@users.noreply.github.com>
2021-02-23 15:55:27 +0200

    Update 20210222_13.c

--- 20210222/20210222_13.c
+++ 20210222/20210222_13.c
@@ -4,3 +4,22 @@
 union <tagUnion> {
 Ctype m_bitField : N;
 };*/
+#include <stdio.h>
+
+#define NUM 6
+#define NUM2 12
+#define NUM3 4
+
+union Uni{
+    short x:NUM;
+    char y:NUM;
+};
+
+int main(){
+    union Uni one;
+    one.x=NUM2;
+    printf("int: %d\n",one.x);
+    one.y=NUM3;
+    printf("char: %c\n",one.y);
+    return 0;
+}
