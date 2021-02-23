27156e6cfe85b95dd461642bf36fbbda8be05cba
bonislava <33098504+bonislava@users.noreply.github.com>
2021-02-23 19:25:24 +0200

    Update 20210219_12.c

--- 20210219/20210219_12.c
+++ 20210219/20210219_12.c
@@ -1,3 +1,23 @@
 /*Задача 12. Напишете функция, която изчислява разстоянието между две
 точки в равнината. Нека функцията да приема два параметъра, които са
 структури ТPoint с два члена X и Y координати.*/
+#include <stdio.h>
+#include <math.h>
+#define NUM1 7
+#define NUM2 10
+#define NUM3 5
+#define NUM4 14
+
+struct TPoint{
+  int X;
+  int Y;
+};
+double distance(struct TPoint a,struct TPoint b){
+    return sqrt(((b.X-a.X)*(b.X-a.X)+(b.Y-a.Y)*(b.Y-a.Y)));
+}
+int main(){
+  struct TPoint a={NUM1,NUM2};
+  struct TPoint b={NUM3,NUM4};    
+  printf("Distance between a and b is %lf", distance(a,b));
+  return 0;
+}
