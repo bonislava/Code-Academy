371e1f754cac437d6fe7c9bece2be9c657f28945
bonislava <33098504+bonislava@users.noreply.github.com>
2021-02-23 19:04:20 +0200

    Update 20210222_15.c

--- 20210222/20210222_15.c
+++ 20210222/20210222_15.c
@@ -3,3 +3,36 @@
 от структури на стандартния изход в CSV формат (
 https://en.wikipedia.org/wiki/Comma-separated_values ). Пример:
 prog1 > structs20.cvs*/
+#include <stdio.h>
+#include <string.h>
+#define SIZE 20
+#define SIZE2 10
+#define D_NUM 0.5
+
+enum bool{
+    false,
+    true
+};
+struct Fields{
+  int num;
+  char arr1[SIZE2];
+  double d;
+  enum bool e;
+};
+int main(){
+  struct Fields arr2[SIZE];
+  for(int i=0;i<SIZE;i++){
+    arr2[i].num=i;
+    strcpy(arr2[i].arr1,"COFFEE");
+    arr2[i].d=(double)i+D_NUM;
+    if(i%2==0){
+        arr2[i].e=true;
+    } else{
+        arr2[i].e=false;
+    }
+  }
+  for(int i=0;i<SIZE;i++){
+    printf("%d,%s,%lf,%d\n", arr2[i].num,arr2[i].arr1,arr2[i].d,arr2[i].e);
+  }
+  return 0;
+}
