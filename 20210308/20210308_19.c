d4b7be7ea926f7ccdc9141cb446a25c3a51ce50a
bonislava <33098504+bonislava@users.noreply.github.com>
2021-03-09 16:58:03 +0200

    Update 20210308_19.c

--- 20210308/20210308_19.c
+++ 20210308/20210308_19.c
@@ -4,3 +4,33 @@
 им на стандартния изход. Ако на програмата не са подадени никакви
 аргументи, то тя да изкара съдържанието подадено от стандартния вход
 на стандартния изход.*/
+
+/*.\a.exe 20210308_15.txt
+  hello*/
+
+#include <stdio.h>
+
+void copy(FILE *first,FILE *second){
+    int c;
+    while((c = getc(first)) != EOF){
+        putc(c,second);
+    }
+}
+int main(int argc,char *argv[]){
+    FILE *fp;
+    if(argc == 1){
+        copy(stdin,stdout);
+    }
+    else{
+        while(--argc > 0){
+            if((fp = fopen(*++argv,"r"))== NULL){
+                printf("cat: can't open %s\n",*argv);
+                return 1;
+            }else{
+                copy(fp,stdout);
+                fclose(fp);
+            }
+        }
+    }
+    return 0;
+}
