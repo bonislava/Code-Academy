3e27b76b2fd7b645bc562fdebc8a4d3d1c4de7d6
bonislava <33098504+bonislava@users.noreply.github.com>
2021-03-21 22:54:43 +0200

    Update task.c

--- individual_task/task.c
+++ individual_task/task.c
@@ -18,18 +18,19 @@ int main(){
     char arr[SIZE],c;
     int i;
     printf("\nPlease Enter any String : \n");
-  	scanf("%s", arr);
-  	lowercase(arr);
+    scanf("%s", arr);
+    lowercase(arr);
     c = find_duplicate(arr);
-  	remove_duplicates(arr,c);
+    remove_duplicates(arr,c);
     uppercase(arr);
     printf("New string is :\n%s \n", arr);
 	
-  	return 0;
+    return 0;
 }
+
 int lowercase(char arr[]){
     int i = 0; 
-    while(arr[i] != '\0') {
+    while(arr[i] != '\0'){
         arr[i] = tolower(arr[i]);
         i++; 
     }
@@ -37,32 +38,33 @@ int lowercase(char arr[]){
     
 }
 void remove_duplicates(char *arr,char c){
-	int i,k=0;
+    int i,k=0;
     for(i=0;arr[i];i++){
      	arr[i]=arr[i+k];     	
      	if(arr[i]==c){
-		  k++;
-		  i--;
+		k++;
+		i--;
         }
     }
 }
+
 char find_duplicate(char *arr){
-	char c='*';
-	int i,j;
+    char c='*';
+    int i,j;
     for(i=0;arr[i];i++){
     	if(!(arr[i]==c)){
-    		for(j=i+1;arr[j];j++){
-            	if(arr[i]==arr[j])
+	    for(j=i+1;arr[j];j++){
+	        if(arr[i]==arr[j])
             	   arr[j]=c;
-		}
-	}	
+	    }
+	}
     }  
-	return c;
-  	 
+    return c;
 }
+
 int uppercase(char arr[]){
     int i = 0;
-    while(arr[i] != '\0') { 
+    while(arr[i] != '\0'){ 
         arr[i] = toupper(arr[i]);
         i++; 
     } 
