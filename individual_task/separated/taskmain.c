1daa1c907914bd24a973bb538e164b67d9ea9653
bonislava <33098504+bonislava@users.noreply.github.com>
2021-03-22 00:55:16 +0200

    Update taskmain.c

--- individual_task/separated/taskmain.c
+++ individual_task/separated/taskmain.c
@@ -4,12 +4,12 @@ int main(){
     char arr[SIZE],c;
     int i;
     printf("\nPlease Enter any String : \n");
-  	scanf("%s", arr);
-  	lowercase(arr);
+    scanf("%s", arr);
+    lowercase(arr);
     c = find_duplicate(arr);
-  	remove_duplicates(arr,c);
+    remove_duplicates(arr,c);
     uppercase(arr);
     printf("New string is :\n%s \n", arr);
 	
-  	return 0;
+    return 0;
 }
