01eb312ad497ac6b8a6c7784ff7a6528b61b6f07
bonislava <33098504+bonislava@users.noreply.github.com>
2021-03-22 01:01:25 +0200

    Update taskfunc.c

--- individual_task/separated/taskfunc.c
+++ individual_task/separated/taskfunc.c
@@ -11,32 +11,28 @@ int lowercase(char arr[]){
 }
 
 void remove_duplicates(char *arr,char c){
-	int i,k=0;
+    int i,k=0;
     for(i=0;arr[i];i++){
-     	arr[i]=arr[i+k];     	
-     	if(arr[i]==c){
-		  k++;
-		  i--;
-        }
+    	arr[i]=arr[i+k];     	
+    	if(arr[i]==c){
+	   k++;
+	   i--;
+	}
     }
 }
 
 char find_duplicate(char *arr){
-	char c='*';
-	int i,j;
+    char c='*';
+    int i,j;
     for(i=0;arr[i];i++){
     	if(!(arr[i]==c)){
-    		for(j=i+1;arr[j];j++){
+    	    for(j=i+1;arr[j];j++){
             	if(arr[i]==arr[j])
             	   arr[j]=c;
-        	
-		    }
-    		
-		}
-     	
-     	
+	    }
+	}
     }  
-	return c;
+    return c;
   	 
 }
 
